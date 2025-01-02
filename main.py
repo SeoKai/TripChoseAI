558f1dbd4f2fa595dc007b4a84c5a0863b6e172a
Seongwan <ssw123c@gamil.com>
2025-01-02 21:49:04 +0900

    .

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import logging
 # Flask 애플리케이션 생성 함수 임포트
 from app import create_app
 
 # Flask 애플리케이션 인스턴스 생성
 app = create_app()
+
+# 로깅 설정
+logging.basicConfig(filename='/home/ec2-user/flask-project/flask.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
+
+@app.route('/')
+def hello_world():
+    app.logger.info('Hello World endpoint was hit.')
+    return 'Hello, World!'
 
 # Python 스크립트가 직접 실행될 경우 Flask 개발 서버 시작
 if __name__ == "__main__":
